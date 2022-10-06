35e256e634b5e3fbecc16dce7b275ce4fc7b7bce
LenaChen <74610301+elenachen1@users.noreply.github.com>
2022-10-06 22:21:26 +0300

    Добавили решение задачи 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,31 @@ while ( caseNumber != 0 ){
    switch(caseNumber)
     {
         case 1:
+            /* 
+            Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+            456 -> 5
+            782 -> 8
+            918 -> 1
+            */
+
+            Console.WriteLine("Задача 1. Данная задача на вход принимает трехзначное число и выводит вторую цифру этого числа");
+            Console.WriteLine("Введите трехзначное число");
+            int number = int.Parse(Console.ReadLine());
+
+            var num = Math.Abs(number);
+            var result = Math.Log10(num) + 1;
+
+            int lenght = (int) result;
+
+            if (lenght == 3)
+            {
+                Console.WriteLine((number%100)/10);
+            }
+
+            else{
+                Console.WriteLine("Число не является трехзначным");
+            }
+
             break;
         case 2:
             break;
@@ -17,7 +42,10 @@ while ( caseNumber != 0 ){
         default:
             break;
 
-    } 
+    }
+    Console.WriteLine("Введите номер задачи от 1 до 4 включительно ");
+    Console.WriteLine("Если вы хотите завершить выполнение программы, то отправте значение 0 ");
+    caseNumber = int.Parse(Console.ReadLine()); 
 }
 
 
