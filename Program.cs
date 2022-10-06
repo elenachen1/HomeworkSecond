fa2bb941a6007ac83d1c2f21381cebc2f8f4ea0d
LenaChen <74610301+elenachen1@users.noreply.github.com>
2022-10-06 21:46:33 +0300

    Создали конструкци. switch case

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите номер задачи от 1 до 4 включительно ");
+Console.WriteLine("Если вы хотите завершить выполнение программы, то отправте значение 0 ");
+
+int caseNumber = int.Parse(Console.ReadLine());
+
+while ( caseNumber != 0 ){
+   switch(caseNumber)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+
+    } 
+}
+
+
+
